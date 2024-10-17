1ece97683ef07088dc8a40cff49579a270a21b25
Asyllzhan <asilzhan.bakytzhanuly@gmail.com>
2024-10-18 05:54:22 +0600

    Asylzhan

---
+++ app/src/main/java/com/example/kinopoisk/ui/theme/tt.kt
@@ -0,0 +1,102 @@
+package com.example.kinopoisk.ui.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+sealed class NavigationItem(val route: String, val icon: ImageVector, val title : String) {
+    object Home : NavigationItem("home", Icons.Filled.Home, "Home")
+    object Search : NavigationItem("search", Icons.Filled.Search, "Search")
+    object Profile : NavigationItem("profile", Icons.Filled.Person, "Profile")
+}
+    @Composable
+    fun BottomNavigationBar(navController: NavController) {
+        val items= listOf(
+            NavigationItem.Home,
+            NavigationItem.Search,
+            NavigationItem.Profile
+        )
+        NavigationBar (containerColor = Color.White){
+            val navBackStackEntry = navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry.value?.destination?.route
+            items.forEach{ item->
+                NavigationBarItem(
+                    icon = {Icon(item.icon, contentDescription = item.title)},
+                    label = { Text(text = item.title)},
+                    selected = currentRoute == item.route,
+                    onClick = { navController.navigate(item.route){
+                        navController.popBackStack(navController.graph.startDestinationId, false)
+                        launchSingleTop = true
+                    } }
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun SetupNavigation(navController: NavHostController) {
+        NavHost(navController, startDestination = NavigationItem.Home.route) {
+            composable(NavigationItem.Home.route) {
+//                HomeScreen()
+            }
+            composable(NavigationItem.Search.route) {
+                SearchScreen()
+            }
+            composable(NavigationItem.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
+
+        @Composable
+        fun SearchScreen(){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "Search Screen")
+            }
+        }
+        @Composable
+        fun ProfileScreen(){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "Profile Screen")
+            }
+        }
+        @Composable
+                fun MainScreen(){
+                    val navController = rememberNavController()
+            Scaffold(
+                bottomBar = { BottomNavigationBar(navController)
+                }
+            )
+            { innerPadding ->
+                Box(modifier = Modifier.padding(innerPadding)) {
+                    SetupNavigation(navController = navController)
+                }
+            }
+                }
+
