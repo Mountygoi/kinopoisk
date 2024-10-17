77e495a01022a6ab3c782238867b11bca1e99b05
Asyllzhan <asilzhan.bakytzhanuly@gmail.com>
2024-10-18 04:21:31 +0600

    Initial Commit

--- app/src/main/java/com/example/kinopoisk/presentation/welcome/WelcomePageViewModule.kt
+++ app/src/main/java/com/example/kinopoisk/presentation/welcome/WelcomePageViewModule.kt
@@ -1,5 +1,12 @@
 package com.example.kinopoisk.presentation.welcome
 
-class WelcomePageViewModule {
+import com.example.kinopoisk.R
+import com.example.kinopoisk.data.WelcomeData
 
+class WelcomePageViewModule {
+  val pages = listOf(
+      WelcomeData(R.drawable.first , "Узнавай \nо премьерах"),
+      WelcomeData(R.drawable.second , "Создавай \nколлекции"),
+      WelcomeData(R.drawable.third , "Делись \nс друзьями")
+  )
 }
