fb4f2fd36b4372f701209040514aa26b76ff66b9
PRONET <musakhanuly00@mail.ru>
2024-11-20 02:25:35 +0500

    Olzhas

---
+++ app/src/main/java/com/example/kinopoisk/data/StaffResponse.kt
@@ -0,0 +1,8 @@
+package com.example.kinopoisk.data
+data class StaffResponse(
+    val staffId : Int?,
+    val nameRu : String?,
+    val posterUrl : String?,
+    val professionKey: String?,
+    val description: String?
+)
